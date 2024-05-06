dfe76ba681efd2d396a27248b354ca73712c6545
Jeremia Kimelman <jeremiak@users.noreply.github.com>
2024-05-06 01:08:37 -0700

    First up

---
+++ sites.mjs
@@ -0,0 +1,61 @@
+/*
+  @typedef Site {
+    name: string
+    url: string
+    category: string,
+    afterWait?: Function
+  }
+*/
+
+// Site[]
+const sites = [
+  {
+    name: 'The Huffington Post',
+    url: 'https://www.huffpost.com/',
+    category: 'Left'
+  },
+  {
+    name: 'Daily Kos',
+    url: 'https://dailykos.com/',
+    category: 'Left'
+  },
+  {
+    name: 'Haaretz',
+    url: 'https://www.haaretz.com/',
+    category: 'Left'
+  },
+  {
+    name: 'CNN',
+    url: 'https://www.cnn.com/',
+    category: 'Center'
+  },
+  {
+    name: 'New York Times',
+    url: 'https://www.nytimes.com/',
+    category: 'Center'
+  },
+  {
+    name: 'NPR',
+    url: 'https://www.npr.org/',
+    category: 'Center'
+  },
+  {
+    name: 'The Drudge Report',
+    url: 'https://www.drudgereport.com/',
+    category: 'Right'
+  },
+  {
+    name: 'Fox News',
+    url: 'https://www.foxnews.com/',
+    category: 'Right'
+  },
+  {
+    name: 'News Max',
+    url: 'https://www.newsmax.com/',
+    category: 'Right'
+  }
+
+  
+]
+
+export default sites
---
+++ update.mjs
@@ -0,0 +1,92 @@
+import fs from "fs/promises";
+
+import _ from "lodash";
+import puppeteer from "puppeteer";
+import Queue from "p-queue";
+import sites from "./sites.mjs";
+
+const browser = await puppeteer.launch();
+const queue = new Queue({ concurrency: 1 });
+
+function wait(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+async function screenshot(site) {
+  console.log("📸", site.name);
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 390,
+    height: 844,
+  });
+  try {
+    await page.goto(site.url, {
+      waitUntil: ["domcontentloaded"],
+    });
+    await wait(2000);
+    await page.screenshot({
+      path: `screenshots/${_.snakeCase(site.name)}.png`,
+    });
+    await page.close();
+  } catch (e) {
+    await page.close();
+    throw e;
+  }
+}
+
+function enqueue(site) {
+  queue.add(async () => {
+    try {
+      await screenshot(site);
+    } catch (e) {
+      console.error(e, site.name);
+      enqueue(site);
+    }
+  });
+}
+
+console.log(`Attempting to take screenshots of ${sites.length} news sites`);
+
+const categories = {};
+
+sites.forEach((site) => {
+  const { category } = site;
+  if (categories[category]) {
+    categories[category].push(site);
+  } else {
+    categories[category] = [site];
+  }
+  enqueue(site);
+});
+
+await queue.onIdle();
+await browser.close();
+
+function formatSiteHtml(site) {
+  const { name } = site;
+  return `<div class="news-site"><div class="news-site-name">${name}</div><div class="news-site-screenshot"><a href="${site.url}"><img src="/screenshots/${
+    _.snakeCase(name)
+  }.png"></a></div></div>`;
+}
+
+const categoriesHtml = Object.keys(categories).map((category) => {
+  const s = categories[category].map((site) => {
+    return formatSiteHtml(site);
+  }).join("");
+  const html =
+    `<div class="category"><strong>${category}</strong><div class="category-sites">${s}</a></div></div>`;
+  return html;
+}).join("");
+
+const html =
+  `<html><head><link rel="stylesheet" href="style.css"></head><body><div class="date">${
+    (new Date()).toDateString()
+  }</div><div class="categories">${categoriesHtml}</div></body></html>`;
+
+await fs.writeFile("index.html", html);
+
+console.log(`🏁 Done`);
